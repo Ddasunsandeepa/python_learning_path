424a5de893bd6425e87aa8c8d9a2bb205283aa3c
John Doe <dsk.sandeep.987@gmail.com>
2025-03-16 14:45:56 +0530

    updated

--- lesson01.py
+++ lesson01.py
@@ -67,3 +67,12 @@ print(bool(gpa))
 input = "22"
 print(type(input))
 print(type(input))
+
+x =1
+y = 1
+print(x == y)
+
+if x == y :
+  print("x is equal to y")
+else :  
+  print("x is not equal to y")
