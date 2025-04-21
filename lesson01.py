a2b5acd3f3f25b769899faf27696c208a9cd8e61
Dasun Sandepa <dsk.sandeep.987@gmail.com>
2025-04-21 08:23:32 +0530

    updated

--- lesson01.py
+++ lesson01.py
@@ -113,3 +113,17 @@ width = float(input("enter the width of the rectangle : "))
 
 area = int(length*width)
 print(f"the area of the rectangle is {area}cm²")
+
+# Madlibs game
+# Word game where you create a story
+# by filling in blanks with random words
+
+adjective1 = input("Enter an adjective (description): ")
+noun1 = input("Enter a noun (person, place, thing): ")
+adjective2 = input("Enter an adjective (description): ")
+verb1 = input("Enter a verb ending with 'ing': ")
+adjective3 = input("Enter an adjective (description): ")
+
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun1}.")
+print(f"{noun1} was {adjective2} and {verb1}.")
